3a08330f6c987f608b4fecf0109aabe0f69deca6
Ask Lootz <sk5040830@stavanger.kommune.no>
2020-12-10 13:06:21 +0100

    Commit

--- scripts/WindowsKeyboard.js
+++ scripts/WindowsKeyboard.js
@@ -4,5 +4,96 @@ $(document).keydown(function(e){
         document.getElementById("key_Esc").style.color = "green";
         document.getElementById("key_Esc").style.border = "solid green";
     }
+    if(e.keyCode == 112){
+        e.preventDefault();
+        document.getElementById("f1").style.color = "green";
+        document.getElementById("f1").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 113){
+        e.preventDefault();
+        document.getElementById("f2").style.color = "green";
+        document.getElementById("f2").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 114){
+        e.preventDefault();
+        document.getElementById("f3").style.color = "green";
+        document.getElementById("f3").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 115){
+        e.preventDefault();
+        document.getElementById("f4").style.color = "green";
+        document.getElementById("f4").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 116){
+        e.preventDefault();
+        document.getElementById("f5").style.color = "green";
+        document.getElementById("f5").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 117){
+        e.preventDefault();
+        document.getElementById("f6").style.color = "green";
+        document.getElementById("f6").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 118){
+        e.preventDefault();
+        document.getElementById("f7").style.color = "green";
+        document.getElementById("f7").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 119){
+        e.preventDefault();
+        document.getElementById("f8").style.color = "green";
+        document.getElementById("f8").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 120){
+        e.preventDefault();
+        document.getElementById("f9").style.color = "green";
+        document.getElementById("f9").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 121){
+        e.preventDefault();
+        document.getElementById("f10").style.color = "green";
+        document.getElementById("f10").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 122){
+        e.preventDefault();
+        document.getElementById("f11").style.color = "green";
+        document.getElementById("f11").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 123){
+        e.preventDefault();
+        document.getElementById("f12").style.color = "green";
+        document.getElementById("f12").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 44){
+        e.preventDefault();
+        document.getElementById("prtscr").style.color = "green";
+        document.getElementById("prtscr").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 145){
+        e.preventDefault();
+        document.getElementById("scroll").style.color = "green";
+        document.getElementById("scroll").style.border = "solid green";
+        return false;
+    }
+    if(e.keyCode == 19){
+        e.preventDefault();
+        document.getElementById("pause").style.color = "green";
+        document.getElementById("pause").style.border = "solid green";
+        return false;
+    }
+
 
 });
