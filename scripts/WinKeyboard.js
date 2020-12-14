0d98a5c275f87759e321a4e823cde95689c3aeef
TotallyNotAsk <asklootz@gmail.com>
2020-12-14 14:25:03 +0100

    Commit

--- scripts/WinKeyboard.js
+++ scripts/WinKeyboard.js
@@ -186,4 +186,229 @@ $(document).keydown(function(event){
         return false;
     }
 
+    //Numpad keys
+    if (event.code === 'NumLock'){
+        event.preventDefault();
+        document.getElementById("NumLock").style.color = "green";
+        document.getElementById("NumLock").style.border = "solid green";
+    }
+    if (event.code === 'NumpadDivide'){
+        event.preventDefault();
+        document.getElementById("numDiv").style.color = "green";
+        document.getElementById("numDiv").style.border = "solid green";
+    }
+    if (event.code === 'NumpadMultiply'){
+        event.preventDefault();
+        document.getElementById("numMult").style.color = "green";
+        document.getElementById("numMult").style.border = "solid green";
+    }
+    if (event.code === 'NumpadSubtract'){
+        event.preventDefault();
+        document.getElementById("numMin").style.color = "green";
+        document.getElementById("numMin").style.border = "solid green";
+    }
+    if (event.code === 'NumpadAdd'){
+        event.preventDefault();
+        document.getElementById("numAdd").style.color = "green";
+        document.getElementById("numAdd").style.border = "solid green";
+    }
+    if (event.code === 'NumpadEnter'){
+        event.preventDefault();
+        document.getElementById("numEnter").style.color = "green";
+        document.getElementById("numEnter").style.border = "solid green";
+    }
+    if (event.code === 'NumpadDecimal'){
+        event.preventDefault();
+        document.getElementById("num.").style.color = "green";
+        document.getElementById("num.").style.border = "solid green";
+    }
+    if (event.code === 'Numpad1'){
+        event.preventDefault();
+        document.getElementById("num1").style.color = "green";
+        document.getElementById("num1").style.border = "solid green";
+    }
+    if (event.code === 'Numpad2'){
+        event.preventDefault();
+        document.getElementById("num2").style.color = "green";
+        document.getElementById("num2").style.border = "solid green";
+    }
+    if (event.code === 'Numpad3'){
+        event.preventDefault();
+        document.getElementById("num3").style.color = "green";
+        document.getElementById("num3").style.border = "solid green";
+    }
+    if (event.code === 'Numpad4'){
+        event.preventDefault();
+        document.getElementById("num4").style.color = "green";
+        document.getElementById("num4").style.border = "solid green";
+    }
+    if (event.code === 'Numpad5'){
+        event.preventDefault();
+        document.getElementById("num5").style.color = "green";
+        document.getElementById("num5").style.border = "solid green";
+    }
+    if (event.code === 'Numpad6'){
+        event.preventDefault();
+        document.getElementById("num6").style.color = "green";
+        document.getElementById("num6").style.border = "solid green";
+    }
+    if (event.code === 'Numpad7'){
+        event.preventDefault();
+        document.getElementById("num7").style.color = "green";
+        document.getElementById("num7").style.border = "solid green";
+    }
+    if (event.code === 'Numpad8'){
+        event.preventDefault();
+        document.getElementById("num8").style.color = "green";
+        document.getElementById("num8").style.border = "solid green";
+    }
+    if (event.code === 'Numpad9'){
+        event.preventDefault();
+        document.getElementById("num9").style.color = "green";
+        document.getElementById("num9").style.border = "solid green";
+    }
+    if (event.code === 'Numpad0'){
+        event.preventDefault();
+        document.getElementById("num0").style.color = "green";
+        document.getElementById("num0").style.border = "solid green";
+    }
+
+    // First letter row
+    if (event.code === 'Tab'){
+        event.preventDefault();
+        document.getElementById("Tab").style.color = "green";
+        document.getElementById("Tab").style.border = "solid green";
+    }
+    if (event.code === 'KeyQ'){
+        event.preventDefault();
+        document.getElementById("q").style.color = "green";
+        document.getElementById("q").style.border = "solid green";
+    }
+    if (event.code === 'Key"'){
+        event.preventDefault();
+        document.getElementById("w").style.color = "green";
+        document.getElementById("w").style.border = "solid green";
+    }
+    if (event.code === 'KeyE'){
+        event.preventDefault();
+        document.getElementById("e").style.color = "green";
+        document.getElementById("e").style.border = "solid green";
+    }
+    if (event.code === 'KeyR'){
+        event.preventDefault();
+        document.getElementById("r").style.color = "green";
+        document.getElementById("r").style.border = "solid green";
+    }
+    if (event.code === 'KeyT'){
+        event.preventDefault();
+        document.getElementById("t").style.color = "green";
+        document.getElementById("t").style.border = "solid green";
+    }
+    if (event.code === 'KeyY'){
+        event.preventDefault();
+        document.getElementById("y").style.color = "green";
+        document.getElementById("y").style.border = "solid green";
+    }
+    if (event.code === 'KeyU'){
+        event.preventDefault();
+        document.getElementById("u").style.color = "green";
+        document.getElementById("u").style.border = "solid green";
+    }
+    if (event.code === 'KeyI'){
+        event.preventDefault();
+        document.getElementById("i").style.color = "green";
+        document.getElementById("i").style.border = "solid green";
+    }
+    if (event.code === 'KeyO'){
+        event.preventDefault();
+        document.getElementById("o").style.color = "green";
+        document.getElementById("o").style.border = "solid green";
+    }
+    if (event.code === 'KeyP'){
+        event.preventDefault();
+        document.getElementById("p").style.color = "green";
+        document.getElementById("p").style.border = "solid green";
+    }
+    if (event.code === 'BracketLeft'){
+        event.preventDefault();
+        document.getElementById("å").style.color = "green";
+        document.getElementById("å").style.border = "solid green";
+    }
+    if (event.code === 'BracketRight'){
+        event.preventDefault();
+        document.getElementById("key_160").style.color = "green";
+        document.getElementById("key_160").style.border = "solid green";
+    }
+    if (event.code === 'Enter'){
+        event.preventDefault();
+        document.getElementById("enter").style.color = "green";
+        document.getElementById("enter").style.border = "solid green";
+    }
+
+    //Second Letter Row
+    if (event.code === 'CapsLock'){
+        event.preventDefault();
+        document.getElementById("caps").style.color = "green";
+        document.getElementById("caps").style.border = "solid green";
+    }
+    if (event.code === 'KeyA'){
+        event.preventDefault();
+        document.getElementById("a").style.color = "green";
+        document.getElementById("a").style.border = "solid green";
+    }
+    if (event.code === 'KeyS'){
+        event.preventDefault();
+        document.getElementById("s").style.color = "green";
+        document.getElementById("s").style.border = "solid green";
+    }
+    if (event.code === 'KeyD'){
+        event.preventDefault();
+        document.getElementById("d").style.color = "green";
+        document.getElementById("d").style.border = "solid green";
+    }
+    if (event.code === 'KeyF'){
+        event.preventDefault();
+        document.getElementById("f").style.color = "green";
+        document.getElementById("f").style.border = "solid green";
+    }
+    if (event.code === 'KeyG'){
+        event.preventDefault();
+        document.getElementById("g").style.color = "green";
+        document.getElementById("g").style.border = "solid green";
+    }
+    if (event.code === 'KeyH'){
+        event.preventDefault();
+        document.getElementById("h").style.color = "green";
+        document.getElementById("h").style.border = "solid green";
+    }
+    if (event.code === 'KeyJ'){
+        event.preventDefault();
+        document.getElementById("j").style.color = "green";
+        document.getElementById("j").style.border = "solid green";
+    }
+    if (event.code === 'KeyK'){
+        event.preventDefault();
+        document.getElementById("k").style.color = "green";
+        document.getElementById("k").style.border = "solid green";
+    }
+    if (event.code === 'KeyL'){
+        event.preventDefault();
+        document.getElementById("l").style.color = "green";
+        document.getElementById("l").style.border = "solid green";
+    }
+    if (event.code === 'Semicolon'){
+        event.preventDefault();
+        document.getElementById("ø").style.color = "green";
+        document.getElementById("ø").style.border = "solid green";
+    }
+    if (event.code === 'Quote'){
+        event.preventDefault();
+        document.getElementById("æ").style.color = "green";
+        document.getElementById("æ").style.border = "solid green";
+    }
+    if (event.code === 'Backslash'){
+        event.preventDefault();
+        document.getElementById("key_222").style.color = "green";
+        document.getElementById("key_222").style.border = "solid green";
+    }
 });
