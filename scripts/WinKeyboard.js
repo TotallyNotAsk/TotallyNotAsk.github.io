ba9b4f7e7e824acc0da71df9f8f7a266a1accde3
TotallyNotAsk <asklootz@gmail.com>
2020-12-14 13:59:01 +0100

    Commit

--- scripts/WinKeyboard.js
+++ scripts/WinKeyboard.js
@@ -65,4 +65,124 @@ $(document).keydown(function(event){
         document.getElementById("f12").style.color = "green";
         document.getElementById("f12").style.border = "solid green";
     }
+
+    //Navigation and edit keys
+    if (event.code === 'PrintScreen'){
+        event.preventDefault();
+        document.getElementById("prt").style.color = "green";
+        document.getElementById("prt").style.border = "solid green";
+    }
+    if (event.code === 'ScrollLock'){
+        event.preventDefault();
+        document.getElementById("scroll").style.color = "green";
+        document.getElementById("scroll").style.border = "solid green";
+    }
+    if (event.code === 'Pause'){
+        event.preventDefault();
+        document.getElementById("pause").style.color = "green";
+        document.getElementById("pause").style.border = "solid green";
+    }
+    if (event.code === 'Insert'){
+        event.preventDefault();
+        document.getElementById("insert").style.color = "green";
+        document.getElementById("insert").style.border = "solid green";
+    }
+    if (event.code === 'Home'){
+        event.preventDefault();
+        document.getElementById("home").style.color = "green";
+        document.getElementById("home").style.border = "solid green";
+    }
+    if (event.code === 'PageUp'){
+        event.preventDefault();
+        document.getElementById("PageUp").style.color = "green";
+        document.getElementById("PageUp").style.border = "solid green";
+    }
+    if (event.code === 'Delete'){
+        event.preventDefault();
+        document.getElementById("del").style.color = "green";
+        document.getElementById("del").style.border = "solid green";
+    }
+    if (event.code === 'End'){
+        event.preventDefault();
+        document.getElementById("end").style.color = "green";
+        document.getElementById("end").style.border = "solid green";
+    }
+    if (event.code === 'PageDown'){
+        event.preventDefault();
+        document.getElementById("PageDown").style.color = "green";
+        document.getElementById("PageDown").style.border = "solid green";
+    }
+
+    //Number keys row
+    if (event.code === 'Backquote'){
+        event.preventDefault();
+        document.getElementById("key_|").style.color = "green";
+        document.getElementById("key_|").style.border = "solid green";
+    }
+    if (event.code === 'Digit1'){
+        event.preventDefault();
+        document.getElementById("1").style.color = "green";
+        document.getElementById("1").style.border = "solid green";
+    }
+    if (event.code === 'Digit2'){
+        event.preventDefault();
+        document.getElementById("2").style.color = "green";
+        document.getElementById("2").style.border = "solid green";
+    }
+    if (event.code === 'Digit3'){
+        event.preventDefault();
+        document.getElementById("3").style.color = "green";
+        document.getElementById("3").style.border = "solid green";
+    }
+    if (event.code === 'Digit4'){
+        event.preventDefault();
+        document.getElementById("4").style.color = "green";
+        document.getElementById("4").style.border = "solid green";
+    }
+    if (event.code === 'Digit5'){
+        event.preventDefault();
+        document.getElementById("5").style.color = "green";
+        document.getElementById("5").style.border = "solid green";
+    }
+    if (event.code === 'Digit6'){
+        event.preventDefault();
+        document.getElementById("6").style.color = "green";
+        document.getElementById("6").style.border = "solid green";
+    }
+    if (event.code === 'Digit7'){
+        event.preventDefault();
+        document.getElementById("7").style.color = "green";
+        document.getElementById("7").style.border = "solid green";
+    }
+    if (event.code === 'Digit8'){
+        event.preventDefault();
+        document.getElementById("8").style.color = "green";
+        document.getElementById("8").style.border = "solid green";
+    }
+    if (event.code === 'Digit9'){
+        event.preventDefault();
+        document.getElementById("9").style.color = "green";
+        document.getElementById("9").style.border = "solid green";
+    }
+    if (event.code === 'Digit0'){
+        event.preventDefault();
+        document.getElementById("0").style.color = "green";
+        document.getElementById("0").style.border = "solid green";
+    }
+    if (event.code === 'Minus'){
+        event.preventDefault();
+        document.getElementById("+").style.color = "green";
+        document.getElementById("+").style.border = "solid green";
+    }
+    if (event.code === 'Equal'){
+        event.preventDefault();
+        document.getElementById("220").style.color = "green";
+        document.getElementById("220").style.border = "solid green";
+    }
+    if (event.code === 'BackSpace'){
+        event.preventDefault();
+        document.getElementById("BackSpace").style.color = "green";
+        document.getElementById("BackSpace").style.border = "solid green";
+    }
+
 });
